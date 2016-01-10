2be75824f18ff3bfa011151dd01e193368474ee6
Bartosz <bartosz.maciej.balcerzak@gmail.com>
2016-01-10 23:56:16 +0100

    added method fixing windows line endings in CubeMX generated files

---
+++ cube2unix.py
@@ -0,0 +1,19 @@
+"""fix files generatied by CubeMX"""
+import os
+from subprocess import call
+
+def dos2unix():
+    """convert Windows dos files to unix"""
+    for _file in os.walk(os.getcwd()):
+        for string in _file[2]:
+            if string.endswith(".c") or \
+               string.endswith(".h") or \
+               string.endswith(".ld") or \
+               string.endswith(".s") or \
+               string.endswith(".launch"):
+                call(["dos2unix", os.path.join(_file[0], string)])
+            else:
+                print "skipping: %s" %string
+
+if __name__ == "__main__":
+    dos2unix()
