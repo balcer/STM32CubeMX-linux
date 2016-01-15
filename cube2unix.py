643e307c7a49bff0254cc26ebe19d1ad73da65de
Bartosz <bartosz.maciej.balcerzak@gmail.com>
2016-01-15 17:30:31 +0100

    moved indent functionality to separate function which affects only *.c files

--- cube2unix.py
+++ cube2unix.py
@@ -12,10 +12,17 @@ def dos2unix():
                string.endswith(".s") or \
                string.endswith(".launch"):
                 call(["dos2unix", os.path.join(_file[0], string)])
-                call(["indent", os.path.join(_file[0], string)])
-                call(["rm", os.path.join(_file[0], string + "~")])
             else:
                 print "skipping: %s" %string
 
+def fix_indent():
+    """fix c file indents"""
+    for _file in os.walk(os.getcwd()):
+        for string in _file[2]:
+            if string.endswith(".c"):
+                call(["indent", os.path.join(_file[0], string)])
+                call(["rm", os.path.join(_file[0], string + "~")])
+
 if __name__ == "__main__":
     dos2unix()
+    fix_indent()
